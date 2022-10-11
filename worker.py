dc9deb53cc3c39d3c2dcbdf5dd0374fd1c058168
Wondaeng <71166964+Wondaeng@users.noreply.github.com>
2022-10-12 04:11:28 +0900

    Add files via upload

---
+++ worker.py
@@ -0,0 +1,122 @@
+import torch
+import cv2, os, time
+import os, cv2, torchvision
+import torchvision.transforms.functional as TF
+import numpy as np
+import mimetypes
+from natsort import natsorted
+
+# Detectron2 for inference
+from detectron2.config import get_cfg
+from detectron2.modeling import build_model
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2 import model_zoo
+
+# You may need to restart your runtime prior to this, to let your installation take effect
+# Some basic setup
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+# import some common detectron2 utilities
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+import os
+
+def build_predictor(weights='model_best.pth', num_classes=1, threshold=0.8):
+    # Loading parasite trained model
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes
+    cfg.MODEL.WEIGHTS = weights
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold
+    predictor = DefaultPredictor(cfg)
+    return predictor
+
+def inference(usr_pth, file_name, save_pth, predictor):
+    # Loading pre-trained model
+    '''weights = FasterRCNN_ResNet50_FPN_Weights.DEFAULT
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights = weights)'''
+    img = cv2.imread(os.path.join(usr_pth, file_name))
+    outputs = predictor(img)
+    v = Visualizer(img[:, :, ::-1], scale=1.0)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    out.save(save_pth)
+    return None
+
+
+def vid_to_fr(usr_pth, n=20):
+    for file in os.listdir(usr_pth):
+        file_path = os.path.join(usr_pth, file)
+        if mimetypes.guess_type(file_path)[0].startswith('video'): # If the file is video
+            
+            video = cv2.VideoCapture(file_path)
+            
+            count = 0
+            success = True
+            while(success):
+                success, image = video.read()
+                if(int(video.get(1)) % n == 0): # extract 1 frame per n frames
+                    save_name = os.path.splitext(file_path)[0]
+                    cv2.imwrite(f'{save_name}_f{count}.jpg', image)
+                    print('Saved frame number :', str(int(video.get(1))))
+                    count += 1
+                
+            video.release()
+            os.system(f"rm {file_path}")
+    return None
+
+    
+'''def draw_boxes(result, orig, save_pth, threshold=0.7):
+    result = result[0]
+    COLORS = np.random.uniform(0, 255, size=(1000, 3))
+    for i in range(0, len(result["boxes"])):
+        confidence = result["scores"][i]
+
+        if confidence > threshold:
+            idx = int(result["labels"][i])
+            box = result["boxes"][i].detach().numpy()
+            (startX, startY, endX, endY) = box.astype("int")
+
+            label = f"{idx}: {confidence*100:.2f}"
+
+            cv2.rectangle(orig, (startX, startY), (endX, endY), COLORS[idx], 3)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(orig, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 1, COLORS[idx], 3)
+
+    cv2.imwrite(f"{save_pth}", orig)
+    return None'''
+
+
+while True:
+    dir_lst = os.listdir('./static/results')
+    for usr in dir_lst:
+        print(usr)
+        print("1")
+        usr_result_pth = os.path.join('./static/results', usr)
+        infer_lst = os.listdir(usr_result_pth)
+        if len(infer_lst) == 0:
+            print("2")
+            usr_data_pth = os.path.join('./static/user_data', usr)
+            vid_to_fr(usr_data_pth)            
+            imgs_data = os.listdir(usr_data_pth)
+            predictor = build_predictor()
+            for img_data in natsorted(imgs_data):
+                print("3")
+                save_pth = os.path.join(usr_result_pth, img_data)
+                outputs = inference(usr_data_pth, img_data, save_pth, predictor)
+        else:
+            pass
+    print('sleep')
+    time.sleep(5)
